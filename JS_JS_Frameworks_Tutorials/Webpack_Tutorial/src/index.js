c6b279c2ab6480df10b75b8824520bbbf502dc7e
Nikolay Bikchentaev <35120296+niko-bikchen@users.noreply.github.com>
2019-08-10 00:42:17 +0300

    Added bootstrap import statement

--- JS_JS_Frameworks_Tutorials/Webpack_Tutorial/src/index.js
+++ JS_JS_Frameworks_Tutorials/Webpack_Tutorial/src/index.js
@@ -2,6 +2,7 @@ import { run } from "./app/app";
 import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 import "./main.scss";
+import "bootstrap";
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
