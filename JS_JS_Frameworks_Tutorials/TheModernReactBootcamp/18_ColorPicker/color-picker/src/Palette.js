90d7eb6b0c6f7b9c8467a92e2be64a09d8c16697
Nikolay Bikchentaev <niko.bikcheb@gmail.com>
2021-01-12 00:21:26 +0200

    Added navbar with a slider to change color brightness

--- JS_JS_Frameworks_Tutorials/TheModernReactBootcamp/18_ColorPicker/color-picker/src/Palette.js
+++ JS_JS_Frameworks_Tutorials/TheModernReactBootcamp/18_ColorPicker/color-picker/src/Palette.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+import Navbar from './Navbar';
 import './Palette.css';
 
 class Palette extends Component {
-    render() {
-        var colorBoxes = this.props.colors.map(function mapColorsToBoxes(background) {
-            return <ColorBox background={background.color} name={background.name} />;
-        });
+  constructor(props) {
+    super(props);
 
-        return (
-            <div className="Palette">
-                <div className="Palette-colors">
-                    {colorBoxes}
-                </div>
-            </div>
-        )
-    }
+    this.state = {
+      level: 500,
+    };
+
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+
+  changeLevel(newLevel) {
+    this.setState({ level: newLevel });
+  }
+
+  render() {
+    var colorBoxes = this.props.palette.colors[this.state.level].map(
+      function mapColorsToBoxes(color) {
+        return <ColorBox background={color.hex} name={color.name} />;
+      }
+    );
+
+    return (
+      <div className="Palette">
+        <Navbar level={this.state.level} changeLevel={this.changeLevel} />
+        <div className="Palette-colors">{colorBoxes}</div>
+      </div>
+    );
+  }
 }
 
 export default Palette;
